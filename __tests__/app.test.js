2ef169ff306816cc0c92e1d57dc4125f32cefc33
Chad Stabler <stablerpsn@gmail.com>
2022-07-25 16:24:23 -0500

    working zodiac routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,22 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../data/zodiacs');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/zodiacs should return a filtered list of zodiac names and ids', async  () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map(zodiac => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
   });
 });
+
+
